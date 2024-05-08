115b87adde3daeb03807fd98866ffa6ff43b7a1e
tjanuki <t.januki@gmail.com>
2024-05-08 16:04:46 -0400

    Add Ticket model

---
+++ database/factories/TicketFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Ticket>
+ */
+class TicketFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => \App\Models\User::factory(),
+            'title' => fake()->sentence,
+            'description' => fake()->paragraph,
+            'status' => fake()->randomElement(['open', 'closed']),
+        ];
+    }
+}
