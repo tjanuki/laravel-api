30dedf935ed7ee8f8ad3d868391edc131f86619d
tjanuki <t.januki@gmail.com>
2024-05-14 14:21:38 -0400

    Add TicketResource

---
+++ app/Http/Resources/V1/TicketResource.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TicketResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'type' => 'ticket',
+            'id' => $this->id,
+            'attributes' => [
+                'title' => $this->title,
+                'description' => $this->description,
+                'status' => $this->status,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at
+            ],
+            'relationships' => [
+                'author' => [
+                    'data' => [
+                        'type' => 'user',
+                        'id' => $this->user_id
+                    ],
+                    'links' => [
+                        'self' => 'todo'
+                    ]
+                ]
+            ],
+            'links' => [
+                [
+                    'rel' => 'self',
+                    'href' => route('api.v1.tickets.store', ['ticket' => $this->id])
+                ]
+            ]
+        ];
+    }
+}
